617ae6a63eae1e0e3f7da6f210e45890c0f3a9a0
geticsen <41274495+geticsen@users.noreply.github.com>
2020-07-03 21:07:12 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+INSERT INTO student(id,`name`,age,sex) VALUES(3,'green',22,'male');
 -- Revise record
-
+UPDATE student SET age=18 WHERE id=3;
 --  Delete record
-
+DELETE FROM student WHERE id=3;
 -- Search record
+SELECT * FROM student WHERE id<3;
